f5940613d9bd8ff49367607dc2d94054ffa90aed
amantirpude <amantirpude79@gmail.com>
2023-11-17 17:00:07 +0530

    delete request done

--- axios_in_Project1/main.js
+++ axios_in_Project1/main.js
@@ -4,13 +4,13 @@ let form = document.getElementById("add-form");
 form.addEventListener('button', onsignup);
 itemList.addEventListener("click",removeItem);
 let myObj_Ser;
-let id =[];
-const createElement = ({name,email,phone,date,time}) => {
+const createElement = (user) => {
   const li = document.createElement('li');
   const deleteBtn = document.createElement('button');
   const editBtn = document.createElement('button');
   li.className = 'list-group';
-  li.innerText = `${name} -> ${email} -> ${phone} -> ${date} -> ${time}`;
+  li.id = user._id;
+  li.innerText = `${user.name} -> ${user.email} -> ${user.phone} -> ${user.date} -> ${user.time}`;
   deleteBtn.className = 'delete';
   editBtn.className = 'edit';
   const deleteText = document.createTextNode('Delete');
@@ -19,39 +19,33 @@ const createElement = ({name,email,phone,date,time}) => {
   editBtn.appendChild(editText);
   li.append(deleteBtn, editBtn);
   itemList.append(li);
-//   deleteItem(deleteBtn,email);
-  editItem(editBtn, name,email,phone,date,time);
+  deleteItem(deleteBtn,user._id);
+  editItem(editBtn, user);
 }
 
-// const deleteItem = (deleteBtn,id) =>{
-//   deleteBtn.onclick = () =>{
-//       deleteDataFromCrudCrud(id);
-//   }
-// }
+const deleteItem = (deleteBtn,id) =>{
+  deleteBtn.onclick = () =>{
+      deleteDataFromCrudCrud(id);
+  }
+}
 
-const editItem = (editBtn,name, email,phone,date,time) =>{
+const editItem = (editBtn,user) =>{
   editBtn.onclick = () =>{
       let itemName = document.getElementById("name");
-      itemName.value = name;
+      itemName.value = user.name;
       let itemEmail = document.getElementById("email");
-      itemEmail.value = email;
+      itemEmail.value = user.email;
       let itemPhone = document.getElementById("phone");
-      itemPhone.value = phone;
+      itemPhone.value = user.phone;
       let itemDate = document.getElementById("date");
-      itemDate.value = date;
+      itemDate.value = user.date;
       let itemTime = document.getElementById("time");
-      itemTime.value = time;
-      localStorage.removeItem(email);
+      itemTime.value = user.time;
   }
 }
 
 function onsignup(event){
     event.preventDefault();
-    // console.log(event.target.name.value);
-    // console.log(event.target.email.value);
-    // console.log(event.target.phone.value);
-    // console.log(event.target.date.value);
-    // console.log(event.target.time.value);
     let myObj = {
         name: event.target.name.value,
         email: event.target.email.value,
@@ -61,68 +55,21 @@ function onsignup(event){
     }
     // myObj_Ser = JSON.stringify(myObj);
     // localStorage.setItem(myObj.email, myObj_Ser);
-
-    //Add Item
-    // console.log(newItem);
-    // Create new li element
-    // let li = document.createElement('li');
-    // Add class
-    // li.className = 'list-group-item';
-    // Add text node with input value
-    // let print = myObj.name + "->" + myObj.email + "->" + myObj.phone + "->" + myObj.date + "->" + myObj.time;
-    // li.appendChild(document.createTextNode(print));
-    // itemList.appendChild(li);
-
-    // Create del button element
-    // let deleteBtn = document.createElement('button');
-  
-    // Add classes to del button
-    // deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
-  
-    // Append text node
-    // deleteBtn.appendChild(document.createTextNode('Delete'));
-    // deleteBtn.onclick = () =>{
-    //   localStorage.removeItem(myObj.email);
-    // }
-
-    //Create Edit Button
-    // let editBtn = document.createElement('button');
-    // editBtn.className = 'edit';
-    // editBtn.appendChild(document.createTextNode('Edit'));
-    // editBtn.onclick = () =>{
-    //   let itemName = document.getElementById("name");
-    //   itemName.value = myObj.name;
-    //   let itemEmail = document.getElementById("email");
-    //   itemEmail.value = myObj.email;
-    //   let itemPhone = document.getElementById("phone");
-    //   itemPhone.value = myObj.phone;
-    //   localStorage.removeItem(myObj.email);
-    // }
-
-    // li.appendChild(editBtn);
-    
-    // Append button to li
-    // li.appendChild(deleteBtn);
-  
-    // Append li to list
-    // itemList.appendChild(li);
-
     postDataToCrudCrud(myObj);
     document.getElementById("name").value = "";
     document.getElementById("email").value = "";
     document.getElementById("phone").value = "";
   }
   
-  window.addEventListener("DOMContentLoaded", () => {
-    axios.get('https://crudcrud.com/api/9cf36211dad648aabf753ec900f321d1/appointmentData')
-      .then((res) => {
-          res.data.forEach(element => {
-              createElement(element);
-              console.log(element);
-          });
-      })
-      .catch((err) => console.log(err))
-  })
+  // window.addEventListener("DOMContentLoaded", () => {
+  //   axios.get('https://crudcrud.com/api/415d78d4d19d4835be848cd04116adc0/appointmentData')
+  //     .then((res) => {
+  //         res.data.forEach(element => {
+  //             createElement(element);
+  //         });
+  //     })
+  //     .catch((err) => console.log(err))
+  // })
   
   // Remove item
   function removeItem(e){
@@ -141,7 +88,7 @@ function onsignup(event){
   }
 
   const postDataToCrudCrud = (myObj) => {
-    axios.post('https://crudcrud.com/api/9cf36211dad648aabf753ec900f321d1/appointmentData?_id=1', myObj)
+    axios.post('https://crudcrud.com/api/415d78d4d19d4835be848cd04116adc0/appointmentData', myObj)
       .then((res) => {
         createElement(res.data);
       })
@@ -149,18 +96,21 @@ function onsignup(event){
   }
 
   const printAllDataFromCrudCrud = () => {
-    axios.get('https://crudcrud.com/api/9cf36211dad648aabf753ec900f321d1/appointmentData')
+    axios.get('https://crudcrud.com/api/415d78d4d19d4835be848cd04116adc0/appointmentData')
         .then((res) => {
             res.data.forEach(element => {
                 createElement(element);
-                console.log(element);
             });
+            console.log(typeof res);
         })
         .catch((err) => console.log(err))
   }
 
-  // printAllDataFromCrudCrud();
+  printAllDataFromCrudCrud();
 
-//   const deleteDataFromCrudCrud = (id) => {
-//     axios.delete(`https://crudcrud.com/api/9cf36211dad648aabf753ec900f321d1/appointmentData/${id}`);
-//   }
+  const deleteDataFromCrudCrud = (id) => {
+    axios.delete(`https://crudcrud.com/api/415d78d4d19d4835be848cd04116adc0/appointmentData/${id}`).then((res) => {
+      console.log(res);
+    })
+    .catch ((err) => console.log(err))
+  }
